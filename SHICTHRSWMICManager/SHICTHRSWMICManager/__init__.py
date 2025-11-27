5c630140dd5a6f6441f67ebdce9af8cbafec95b2
JNTMTMTM <15251865468@163.com>
2025-11-27 16:43:56 +0800

    V0.0.0 添加WMIC可用性检查函数及异常处理

--- SHICTHRSWMICManager/SHICTHRSWMICManager/__init__.py
+++ SHICTHRSWMICManager/SHICTHRSWMICManager/__init__.py
@@ -9,6 +9,8 @@ import os
 from colorama import init
 init()
 
+from .utils.SHRWMICManager_check_is_wmic_available import check_is_wmic_available
+
 print('\033[1mWelcome to use SHRWMICManager\033[0m\n|  \033[1;34mGithub : https://github.com/JNTMTMTM/SHICTHRS_WMICManager\033[0m')
 print('|  \033[1mAlgorithms = rule ; Questioning = approval\033[0m')
 print('|  \033[1mCopyright : © 2025-2026 SHICTHRS, Std. All rights reserved.\033[0m\n')
@@ -19,3 +21,9 @@ class SHRWMICManagerException(BaseException):
     
     def __str__(self):
         return self.message
+
+def SHRWMICManager_check_is_wmic_available():
+    try:
+        return check_is_wmic_available()
+    except Exception as e:
+        raise SHRWMICManagerException(f"SHRWMICManagerException [ERROR.7000] unable to check wmic | {str(e)}")
